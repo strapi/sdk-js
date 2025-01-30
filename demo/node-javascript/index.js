b2b84556abd14bdd3db0772bbcdee16a8571cf70
Ben Irvin <ben@innerdvations.com>
2025-01-30 12:45:35 +0100

    revert: remove unintended files

--- demo/node-javascript/index.js
+++
@@ -1,9 +0,0 @@
-import * as sdk from '@strapi/sdk-js';
-const { strapi } = sdk;
-
-strapi({
-  baseURL: 'http://localhost:1337',
-  token: 'strapi-token',
-});
-
-const res = await strapi.console.log(res);
